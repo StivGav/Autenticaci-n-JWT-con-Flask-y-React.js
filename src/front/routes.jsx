f8ef11747a8e37f8ccfb6b06dc9fa41d7a4ff63d
Stiv Brandon Gaviria <brandonbsgr@gmail.com>
2026-04-15 18:25:31 +0000

    Arreglo de las rutas importadas

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -9,6 +9,9 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
+import { Signup } from "./pages/Signup";
+import { Login } from "./pages/Login";
+import { Private } from "./pages/Private";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
